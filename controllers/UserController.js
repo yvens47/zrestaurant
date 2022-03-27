e8d026cf4920bacd03b5aa5bf080ebf9abb5cc42
Jean Pierre <yvens47@yahoo.com>
2022-03-27 20:21:28 +0000

    users login and registration

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,14 +3,27 @@
 const Users = require("../models/users.model");
 
 
-
+// login user
 const Login = model => async (req, res, next) => {
-  const { email } = req.body;
-  const found = model.filter((user) => user.email === email)[0];
-  // user not found redirect back to login
-  if (!found)
-    return res.json({ error: "wrong login" });
-  res.json({ success: true, message: "Logged in " })
+  try {
+    const { email } = req.body;
+
+    const user = await Users.findOne({ email: email }).exec();
+    if (!user) {
+      return res.status(400).send({ message: "The username does not exist" });
+    }
+    // session and jwt
+    const {_id, about, username} = user;
+    res.json(
+      { data:{isLogged:true}, success: true, message: "Logged in ", user:{email, _id, about, username} });
+
+  } catch (e) {
+    console.log(e);
+    res.json(e);
+    
+
+  }
+
 
 
 }
@@ -21,15 +34,24 @@ const Register = model => async (req, res, next) => {
     const { email, password } = req.body;
     if (email !== '' && password !== '' && password.length >= 8) {
 
-      // create new user but first hash passwords
+      // check if user already existed
+      const oldUser = await Users.findOne({email:email});
+      if(oldUser) return res.json({message:"Account already existed"});
 
+      // create new user but first hash passwords
       const user = new Users({ email: email, password: password });
+      
       user.save(function(err) {
         if (err) {
           console.log(err);
-          return;
+          // type of error
+          let errorMsg = '';
+          
+          if (err.code == 11000 && err.keyPattern.email == 1) {
+            errorMsg = "Email already registered";
+          }
+          return res.json({ error: errorMsg });
         }
-
         res.json({ message: "You have registered successfully", success: true })
       });
 
--- models/users.model.js
+++ models/users.model.js
@@ -1,32 +1,28 @@
 
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 const Bcrypt = require('bcrypt');
 const saltRounds = 10;
+// user schema
+const userSchema = new Schema({
+  username: { type: String, default: "" }, // String is shorthand for {type: String}
+  password: { type: String, required: true, minLength: 8 },
+  about: String,
+  email: { type: String, unique: true },
+  token: String
 
 
+});
 
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = Bcrypt.hashSync(this.password, saltRounds);
+  next();
 
-  const userSchema = new Schema({
-    username:  String, // String is shorthand for {type: String}
-    password: String,
-    about:   String,
-    email:{type:String, unique:true},
-   
-   
-  });
+});
+userSchema.methods.comparePassword = function(plaintext, callback) {
+  return callback(null, Bcrypt.compareSync(plaintext, this.password));
+};
 
-  userSchema.pre('save', function(next){
-
-    if(!this.isModified('password')) return next();
-    this.password = Bcrypt.hashSync(this.password,saltRounds);
-    next();
-    
-    
-  });
-userSchema.methods.comparePassword = function(plaintext, callback) {};
-
-  const User = mongoose.model('User', userSchema);
-
-
+const User = mongoose.model('User', userSchema);
 module.exports = User;
